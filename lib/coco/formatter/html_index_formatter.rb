f5c31e61488ff06f438bb8f17302e97e9f865da8
Andrew Retmanski <andrew@smoothcontract.com>
2013-09-27 17:39:51 +0100

    Tweaked implementation for average stats calculation This passes specs now, although need to add some more specs for this functionality

--- lib/coco/formatter/html_index_formatter.rb
+++ lib/coco/formatter/html_index_formatter.rb
@@ -14,6 +14,7 @@ module Coco
       @template = Template.open File.join($COCO_PATH,'template/index.erb')
       @lines = []
       build_lines_for_context
+      build_stats_for_context
     end
     
     def format
@@ -31,7 +32,12 @@ module Coco
       end
       @lines.sort!
     end
-    
+
+    def build_stats_for_context
+      @total_files = @uncovered.count + @lines.count  
+      @total_coverage = @lines.inject(0) {|total, line| total + line.first }
+      @average_coverage = @total_files > 0 ? @total_coverage / @total_files : 0
+    end
   end
 
 end
